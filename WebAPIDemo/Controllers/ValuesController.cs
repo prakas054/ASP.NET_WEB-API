e0300ac1613e52e123bc5bdc461dd8b223280972
prakash kumar <prakas054@gmail.com>
2019-01-24 02:36:57 +0530

    first commit of .net

---
+++ WebAPIDemo/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIDemo.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        static List<string> str = new List<string>()
+        {
+            "value0", "value1", "value2", "prakash"
+        };
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return str;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return str[id];
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+            str.Add(value);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+            str[id] = value;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            str.RemoveAt(id);
+        }
+    }
+}
